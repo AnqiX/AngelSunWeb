598a0935fb07258cc7deb0d0bb6e38acc2f6c0dc
Angel.Sun <angel.sun@goodmanmfg.com>
2017-07-09 23:40:18 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<title>Angel Sun - Personal Website</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="cache-control" content="max-age=0" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta http-equiv="expires" content="0" />
+<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+<meta http-equiv="pragma" content="no-cache" />
+<link rel="stylesheet" href="css/w3.css">
+<link rel="stylesheet" href="css/style.css">
+<body>
+
+<div style = 'position: absolute; height: 100%; width: 100%;' class = 'w3-display-container'>
+	<div class="vector right w3-text-white" >
+		<div>
+			<svg viewBox="0 0 40 100" preserveAspectRatio="none">
+				<polygon points="30,0 40,0 40,100 10,100" />
+			</svg>
+
+			<div class = 'w3-display-topleft w3-padding-32 w3-container w3-half w3-center' style = 'margin-top: 3%'>
+				<h1 class = 'w3-margin-bottom'>Angel Sun</h1>
+				<div class = 'w3-center w3-margin '>
+					<img src="images/portrait.jpg" style = 'height: 170px; width: 165px; border-radius: 50%'>
+				</div>
+				<h4>Programmer | Engineer | Learner</h4>
+				<p class = 'w3-quarter'></p>
+				<p class ='w3-half mobileHide' style = 'color: #D7DDDD; line-height: 20px'>I am an energetic and enthusiastic person who enjoys a challenge and achieving personal goals. My passion lies in computer science. The oppotunity to work with new technologies and develop fun projects is particularly attractive to me.</p>
+			</div>
+			<div class = 'w3-display-bottomright w3-padding-32 w3-container w3-half w3-center' style = 'margin-bottom: 10%' >
+				<a href = '#resume'><h3>Resume</h3></a>
+				<h3>Fun projects</h3>
+				<h3>Hobbies</h3>
+				<h3>Keep in touch</h3>
+			</div>
+			
+		</div>
+	</div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="js/index.js"></script>
+
+</body>
+</html> 
